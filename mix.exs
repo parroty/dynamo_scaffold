1dfaff7f553d146b2ba90b9bb1a5f113c0a5aee8
parroty <parroty00@gmail.com>
2013-12-01 22:43:18 +0900

    Update dependency for elixir/master

--- mix.exs
+++ mix.exs
@@ -22,7 +22,7 @@ defmodule DynamoScaffold.Mixfile do
       { :cowboy, github: "extend/cowboy" },
       { :dynamo, "0.1.0-dev", github: "elixir-lang/dynamo"},
       { :ecto, github: "elixir-lang/ecto" },
-      { :pgsql, github: "ericmj/pgsql", branch: "elixir" }
+      { :postgrex, github: "ericmj/postgrex" }
     ]
   end
 end
